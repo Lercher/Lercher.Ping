fe629af1c05d6f0a414449300a80cd11f3691e4f
Martin Lercher <lercher@singhammer.de>
2017-11-23 18:18:33 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lercher.Ping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Lercher.Ping, (c) 2017 by Cassiopae Gmbh, M. Lercher");
+            
+        }
+    }
+}
